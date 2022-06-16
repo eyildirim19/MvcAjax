76f25939dd8e8b4b03568e158282c9eb03cca6aa
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-06-16 21:46:15 +0300

    Add project files.

---
+++ MvcAjax/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int Sayac
+        {
+            get
+            {
+                return HttpContext.Session.GetInt32("syc") == null ? 0 : (int)HttpContext.Session.GetInt32("syc");
+            }
+            set
+            {
+                HttpContext.Session.SetInt32("syc", value);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View(Sayac);
+        }
+
+        [HttpPost]
+        public IActionResult Arttir()
+        {
+            Sayac++;
+
+            return Json(Sayac); // geriye json dön..
+        }
+    }
+}
---
+++ MvcAjax/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
